c6b7ca204af8fdfec591e9ee2c15ef7ed758d93f
Ivan Gonzalez <idgonzaleze@gmail.com>
2020-09-19 16:03:40 -0500

    prettier/lint

--- app/modules/stament/services/stament.service.js
+++ app/modules/stament/services/stament.service.js
@@ -12,112 +12,98 @@ module.exports = class stamentModuleService extends baseService {
 		this.updateStaments = {}
 	}
 
-	findListKey(items){
-
+	findListKey(items) {
 		const keys = Object.keys(items)
-		const listKeyStr = keys.find(key => Array.isArray(items[key]) )
-		return listKeyStr;
+		const listKeyStr = keys.find(key => Array.isArray(items[key]))
+		return listKeyStr
 	}
 
-	setStament(operation = "add", key ='', stamentValue = ''){
-
-		console.log(" setStament ", operation, key, stamentValue)
+	setStament(operation = 'add', key = '', stamentValue = '') {
+		console.log(' setStament ', operation, key, stamentValue)
 
 		const operationTypeKeyStr = `$${operation}`
-		if(!this.updateStaments[operationTypeKeyStr])
-			this.updateStaments[operationTypeKeyStr] = {}
+		if (!this.updateStaments[operationTypeKeyStr]) this.updateStaments[operationTypeKeyStr] = {}
 
-		switch(operation){
-			case "add":
+		switch (operation) {
+			case 'add':
 				this.updateStaments[operationTypeKeyStr][key] = [stamentValue]
-			break;
-			case "update":
+				break
+			case 'update':
 				this.updateStaments[operationTypeKeyStr][key] = stamentValue
-			break;
-			case "remove":
+				break
+			case 'remove':
 				this.updateStaments[operationTypeKeyStr][key] = stamentValue
-			break
+				break
 			default:
 				this.updateStaments[operationTypeKeyStr][key] = [stamentValue]
-			break; 
+				break
 		}
-
 	}
 
 	async generateUpdateStatement(params) {
-
-
-		const originalDocument = params.original;
-		const mutations = params.mutation;
+		const originalDocument = params.original
+		const mutations = params.mutation
 
 		//Get the key with the array inside
-		const arrayKeyStr  = Object.keys(originalDocument).find(key => Array.isArray( originalDocument[key])  )
+		const arrayKeyStr = Object.keys(originalDocument).find(key =>
+			Array.isArray(originalDocument[key])
+		)
 
-		try{
-
-			const mutationList = mutations[arrayKeyStr];
-			mutationList.forEach(mutation => this.deepIterator(originalDocument[arrayKeyStr], mutation, arrayKeyStr) )
+		try {
+			const mutationList = mutations[arrayKeyStr]
+			mutationList.forEach(mutation =>
+				this.deepIterator(originalDocument[arrayKeyStr], mutation, arrayKeyStr)
+			)
 			return this.updateStaments
-
-		}catch(e){
+		} catch (e) {
 			Promise.reject(e)
 		}
-
 	}
 
-	deepIterator(mainList = [], mutationDoc= {}, parentKey = null){
+	deepIterator(mainList = [], mutationDoc = {}, parentKey = null) {
+		try {
+			mainList.forEach((listItem, mainIndex) => {
+				const listKeyStr = this.findListKey(listItem)
 
-		try{
-
-			mainList.forEach((listItem, mainIndex)=>{
-				
-				const listKeyStr = this.findListKey(listItem) 
-
-				if(!mutationDoc._id){
-
-					this.setStament("add", parentKey, mutationDoc )
-					return 
-
-				}else{
-
-					if(listItem._id === mutationDoc._id  ){
-
-						if(mutationDoc._delete){
-
+				if (!mutationDoc._id) {
+					this.setStament('add', parentKey, mutationDoc)
+					return
+				} else {
+					if (listItem._id === mutationDoc._id) {
+						if (mutationDoc._delete) {
 							let key = `${parentKey}.${mainIndex}`
-							this.setStament("remove", key, true )
+							this.setStament('remove', key, true)
 
-							return;
+							return
 						}
 
 						let mutationKeys = Object.keys(mutationDoc)
-						mutationKeys = mutationKeys.filter(key => { return key != "_id"})
-						const mutationListKeyStr = mutationKeys.find(key => Array.isArray(mutationDoc[key]) )
+						mutationKeys = mutationKeys.filter(key => {
+							return key != '_id'
+						})
+						const mutationListKeyStr = mutationKeys.find(key => Array.isArray(mutationDoc[key]))
 
 						console.log(this)
-						mutationKeys.forEach(k =>{
-
-
-							if( !Array.isArray(mutationDoc[k]) ){
-								this.setStament("update", `${parentKey}.${mainIndex}.${k}`, mutationDoc[k] )
+						mutationKeys.forEach(k => {
+							if (!Array.isArray(mutationDoc[k])) {
+								this.setStament('update', `${parentKey}.${mainIndex}.${k}`, mutationDoc[k])
 							}
 						})
-						 
-						if(mutationListKeyStr){	
 
-							mutationDoc[mutationListKeyStr].forEach((mutationItemList)=>{
-								this.deepIterator(  listItem[listKeyStr], mutationItemList, `${parentKey}.${mainIndex}.${mutationListKeyStr}`)
+						if (mutationListKeyStr) {
+							mutationDoc[mutationListKeyStr].forEach(mutationItemList => {
+								this.deepIterator(
+									listItem[listKeyStr],
+									mutationItemList,
+									`${parentKey}.${mainIndex}.${mutationListKeyStr}`
+								)
 							})
 						}
-	 
-
 					}
-
 				}
 			})
-		}catch(e){
+		} catch (e) {
 			return e
 		}
-
 	}
 }
